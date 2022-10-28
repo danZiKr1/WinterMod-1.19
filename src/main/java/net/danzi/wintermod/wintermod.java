4712159d39485e6f12e7308b3d21f1920ce35fda
danZiKr1 <daniilbarbuk2@gmail.com>
2022-10-28 21:20:18 +0300

    project started

---
+++ src/main/java/net/danzi/wintermod/wintermod.java
@@ -0,0 +1,15 @@
+package net.danzi.wintermod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class wintermod implements ModInitializer {
+	public static final String MOD_ID = "wintermod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/danzi/wintermod/wintermodclient.java
@@ -0,0 +1,10 @@
+package net.danzi.wintermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class wintermodclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
